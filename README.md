533609557f3ed5a6a32f1e66bbe28b7883f909b3
Patryk Zabicki <patryk.zabicki@numbrs.com>
2019-06-21 10:37:15 +0200

    Adding sync script

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Collection of various dotfiles configurations.
 
+# Usage
+
+Just run the run.sh and restart the shell or source .bashrc.
+Cnosecutive runs will source the .bashrc automatically.
+
 
---
+++ run.sh
@@ -0,0 +1,11 @@
+cp .bash_aliases $HOME/.bash_aliases
+cp .bash_profile $HOME/.bash_profile
+cp .bashrc $HOME/.bashrc
+cp .git-completion $HOME/.git-completion
+cp .git-prompt $HOME/.git-prompt
+cp .gitconfig $HOME/.gitconfig
+cp .gitignore_global $HOME/.gitignore_global
+cp .ps1 $HOME/.ps1
+cp .ps1-constants $HOME/.ps1-constants
+cp .vimrc $HOME/.vimrc
+cp -R .vim $HOME/.vim
